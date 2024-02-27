cc000b4ef98cc40d32ed721a3c9dd3c49dd0ddc3
Tae Kyoung Lee <122856705+taekyounglee1224@users.noreply.github.com>
2024-02-27 15:40:25 +0900

    Delete Web Python Programming (SWCON104)/main.py

--- Web Python Programming (SWCON104)/main.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-Handler = http.server.SimpleHTTPRequestHandler
-PORT = 80
-
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
